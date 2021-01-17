cf6db089114e2f2471d29abc9095899f6fff501a
muhsina-abdulla <muhsinabinthabdulla@gmail.com>
2021-01-17 20:47:23 +0530

    Makes a small change on Outlet.cs.
    Removes a property from Outlet class,Makes a new migration.

--- GothamCaresWebApp/BuisnessServices/Migrations/20210117150600_SecondDB.Designer.cs
+++ GothamCaresWebApp/BuisnessServices/Migrations/20210117150600_SecondDB.Designer.cs
@@ -2,15 +2,17 @@
 using BuisnessServices.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 namespace BuisnessServices.Migrations
 {
     [DbContext(typeof(GothamContext))]
-    partial class GothamContextModelSnapshot : ModelSnapshot
+    [Migration("20210117150600_SecondDB")]
+    partial class SecondDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ GothamCaresWebApp/BuisnessServices/Migrations/20210117150600_SecondDB.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BuisnessServices.Migrations
+{
+    public partial class SecondDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Volunteers_Outlets_OutletId",
+                table: "Volunteers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Volunteers_OutletId",
+                table: "Volunteers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Volunteers_OutletId",
+                table: "Volunteers",
+                column: "OutletId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Volunteers_Outlets_OutletId",
+                table: "Volunteers",
+                column: "OutletId",
+                principalTable: "Outlets",
+                principalColumn: "OutletId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
