a54c5cd72fbbafa5b56e847252907fb5ef49ce8b
muhsina-abdulla <muhsinabinthabdulla@gmail.com>
2021-01-23 17:09:41 +0530

    Adds Migrations

--- GothamCaresWebApp/DataService/Data/GothamContext.cs
+++ GothamCaresWebApp/DataService/Data/GothamContext.cs
@@ -25,6 +25,7 @@ namespace DataService.Data
             modelBuilder.Entity<Outlet>().HasData(
               new Outlet
               {
+                  OutletId = 1,
                   Name = "Rose",
                   Street = "ABC",
                   Landmark = "ABC Store",
@@ -35,6 +36,7 @@ namespace DataService.Data
               }
               ,new Outlet
               {
+                  OutletId = 2,
                   Name = "Lily",
                   Street = "DEF",
                   Landmark = "DEF Store",
@@ -47,7 +49,7 @@ namespace DataService.Data
 
              , new Outlet
              {
-
+                 OutletId = 3,
                  Name = "Jasmine",
                  Street = "GHI",
                  Landmark = "GHI Store",
@@ -58,7 +60,7 @@ namespace DataService.Data
              }, new Outlet
              {
 
-
+                 OutletId = 4,
                  Name = "Lavender",
                  Street = "JKL",
                  Landmark = "JKL Store",
@@ -70,7 +72,7 @@ namespace DataService.Data
              }, new Outlet
              {
 
-
+                 OutletId = 5,
                  Name = "Orchid",
                  Street = "MNO",
                  Landmark = "MNO Store",
@@ -80,18 +82,18 @@ namespace DataService.Data
                  Date = "25/01/2020"
 
              }
-            ) ; 
+            ) ;
 
 
             modelBuilder.Entity<Volunteers>().HasData(new Volunteers
             {
-               
+                Id = 1,
                 Name = "Muhsina",
                 Phone = 86066244485,
                 email = "muhsinabinthabdulla@gmail.com",
                 OutletId = 1,
-                
-                
+
+
 
             }, new Volunteers
             {
@@ -100,34 +102,34 @@ namespace DataService.Data
                 Phone = 98204928902,
                 email = "Hepzibha@gmail.com",
                 OutletId = 2,
-                
+
 
             }, new Volunteers
             {
-                
+                Id = 3,
                 Name = "Priya",
                 Phone = 5864564025,
                 email = "Prya@gmail.com",
                 OutletId = 3,
-                
+
             }, new Volunteers
             {
-                
+                Id = 4,
                 Name = "Megha",
                 Phone = 89208374890,
                 email = "Megha@gmail.com",
                 OutletId = 4,
-                
+
             }, new Volunteers
             {
-                
+                Id = 5,
                 Name = "Navneeth",
                 Phone = 9820328902,
                 email = "FantomPhreak@gmail.com",
                 OutletId = 5,
-               
+
             }
-            ); ;
+            ); 
 
             modelBuilder.Entity<Admin>().HasData(new Admin
             {
---
+++ GothamCaresWebApp/DataService/Migrations/20210123113717_initialDB.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace DataService.Migrations
+{
+    public partial class initialDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Admins",
+                columns: table => new
+                {
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Admins", x => x.Email);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Outlets",
+                columns: table => new
+                {
+                    OutletId = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(15)", maxLength: 15, nullable: false),
+                    Street = table.Column<string>(type: "text", nullable: false),
+                    Landmark = table.Column<string>(type: "text", nullable: false),
+                    FoodPackets = table.Column<int>(type: "integer", nullable: false),
+                    FoodType = table.Column<int>(type: "integer", nullable: false),
+                    RequiredVolunteers = table.Column<int>(type: "integer", nullable: false),
+                    Date = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Outlets", x => x.OutletId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Volunteers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    Phone = table.Column<long>(type: "bigint", nullable: false),
+                    email = table.Column<string>(type: "text", nullable: false),
+                    OutletId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Volunteers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Volunteers_Outlets_OutletId",
+                        column: x => x.OutletId,
+                        principalTable: "Outlets",
+                        principalColumn: "OutletId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Admins",
+                columns: new[] { "Email", "Password" },
+                values: new object[,]
+                {
+                    { "abcd@gmail.com", "abcd" },
+                    { "efgh@gmail.com", "efgh" },
+                    { "ijkl@gmail.com", "ijkl" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Outlets",
+                columns: new[] { "OutletId", "Date", "FoodPackets", "FoodType", "Landmark", "Name", "RequiredVolunteers", "Street" },
+                values: new object[,]
+                {
+                    { 1, "21/01/2020", 78, 1, "ABC Store", "Rose", 7, "ABC" },
+                    { 2, "22/01/2020", 100, 0, "DEF Store", "Lily", 10, "DEF" },
+                    { 3, "23/01/2020", 110, 2, "GHI Store", "Jasmine", 11, "GHI" },
+                    { 4, "24/01/2020", 200, 2, "JKL Store", "Lavender", 20, "JKL" },
+                    { 5, "25/01/2020", 400, 0, "MNO Store", "Orchid", 40, "MNO" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Volunteers",
+                columns: new[] { "Id", "Name", "OutletId", "Phone", "email" },
+                values: new object[,]
+                {
+                    { 1, "Muhsina", 1, 86066244485L, "muhsinabinthabdulla@gmail.com" },
+                    { 2, "Hepzibha", 2, 98204928902L, "Hepzibha@gmail.com" },
+                    { 3, "Priya", 3, 5864564025L, "Prya@gmail.com" },
+                    { 4, "Megha", 4, 89208374890L, "Megha@gmail.com" },
+                    { 5, "Navneeth", 5, 9820328902L, "FantomPhreak@gmail.com" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Volunteers_OutletId",
+                table: "Volunteers",
+                column: "OutletId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Admins");
+
+            migrationBuilder.DropTable(
+                name: "Volunteers");
+
+            migrationBuilder.DropTable(
+                name: "Outlets");
+        }
+    }
+}
